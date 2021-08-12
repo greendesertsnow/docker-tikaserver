c3025353ff69bdf5152383d9d8279a696bf14b9d
greendesertsnow <magic3reality@gmail.com>
2021-08-12 19:22:23 +0300

    sh spacing -_-

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -4,7 +4,7 @@ TEMPLATE_FILE=/tika-config.template.xml
 GENERATE_FILE=/tika-config.xml
 GENERATE_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-sed "s/GENERATE_DATE/${GENERATE_DATE}/g" ${TEMPLATE_FILE} | \   
+sed "s/GENERATE_DATE/${GENERATE_DATE}/g" ${TEMPLATE_FILE} | \
     sed "s/TIKA_TEXT_CSV_CONFIDENCE/${TIKA_TEXT_CSV_CONFIDENCE:-0.5}/g" | \
     sed "s/TIKA_HTML_EXTRACT_SCRIPTS/${TIKA_HTML_EXTRACT_SCRIPTS:-false}/g" | \
     sed "s/TIKA_OFFICE_EXTRACT_MACROS/${TIKA_OFFICE_EXTRACT_MACROS:-false}/g" | \
